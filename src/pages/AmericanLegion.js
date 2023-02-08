a4bc54e03c2a0e9f8602a1c40c110823559ae547
Frank <frankamalafronte@gmail.com>
2023-02-08 09:27:32 -0500

    add 7 posts, change width

--- src/pages/AmericanLegion.js
+++ src/pages/AmericanLegion.js
@@ -11,8 +11,15 @@ export default function AmericanLegion(props) {
 return (
   <Links>
   <body>
-  <div style={{ display: 'flex', justifyContent: 'center' }}>
-  <InstagramEmbed url="https://www.instagram.com/p/CoTi8DDuX7R/" width={600} />
+  <div style={{ display: 'flex', alignItems: 'center', flexDirection:'column' }}>
+  <InstagramEmbed url="https://www.instagram.com/p/CoYp0mDO8Rb/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoYpV-VuTYr/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoTi8DDuX7R/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoX-WNBvUGO/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoX90MLvQpk/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoX9OGov5oJ/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoX8JYLvV5a/" width={'75%'} />
+  <InstagramEmbed url="https://www.instagram.com/p/CoX7IVovC2-/" width={'75%'} />
   </div>
   </body>
     </Links>
